48d28d74e6b5473ab8121fdeec0334537bb31144
amnasyeda <amna.syeda98@gmail.com>
2021-06-04 08:18:13 -0400

    js history should store in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 // Variables 
-var searchBtn = document.querySelector('#searchBtn');
-var mainSearch = document.querySelector('#mainSearch');
-var userInput = document.querySelector('userInput');
-var dailyForcast = document.querySelector('dailyForcast');
+var searchBtn = document.querySelector(".searchBtn");
+var mainSearch = document.querySelector(".mainSearch");
+var userInput = document.querySelector(".userInput");
+var dailyForcast = document.querySelector(".dailyForcast");
+var nameofCity = document.querySelector(".cityName");
+var currentDay = document.querySelector(".curentDay");
+var weatherIcon = document.querySelector(".weatherIcon");
+var previousSearches = document.querySelector(".prevsearches")
+var temperature = document.querySelector(".temp");
+var humidity = document.querySelector(".humidity");
+var windSpeed = document.querySelector(".windSpeed");
+var uvIndex = document.querySelector(".uvIndex");
+var cardRow = document.querySelector(".cardRow");
 
 // Save to local storage
 var storage = JSON.parse(localStorage.getItem('prevsearches'));
+
+// Current date display
 
 
 
